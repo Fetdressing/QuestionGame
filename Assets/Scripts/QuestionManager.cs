497a764ed8a33e278cc1fdb7db0b4afb65fa876c
Simon Hyltegard <simon.home@hotmail.com>
2020-11-18 10:44:55 +0100

    Started working on question manager, for loading and saving questions

--- Assets/Scripts/QuestionManager.cs
+++ Assets/Scripts/QuestionManager.cs
@@ -2,17 +2,56 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+using System.IO;
+using System.Linq;
+
 public class QuestionManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private List<string> currQuestionList = new List<string>();
+
+    public void Add()
     {
-        
+
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Remove(int index)
     {
-        
+
+    }
+
+    public List<string> GetQuestions()
+    {
+        return currQuestionList;
+    }
+
+    public void Save(string fileName)
+    {
+        StreamWriter sw = new StreamWriter(fileName, true);
+        for (int i = 0; i < currQuestionList.Count; i++)
+        {
+            sw.Write(currQuestionList[i]);
+        }
+
+        sw.Close();
+    }
+
+    public void Load(string fileName)
+    {
+        if (!File.Exists(fileName))
+        {
+            Debug.Log("File didn't exist: " + fileName);
+            return; // No questions to load.
+        }
+
+        currQuestionList.Clear();
+        StreamReader reader = new StreamReader(fileName);
+
+        while (!reader.EndOfStream)
+        {
+            string question = reader.ReadLine();
+            currQuestionList.Add(question);
+        }
+
+        reader.Close();
     }
 }
