dfe9dc9a315b4cd6432d9a846508a8467982a665
Simon Hyltegard <simon.home@hotmail.com>
2020-11-24 10:57:57 +0100

    Adding scenes

---
+++ Assets/Scripts/UI/MainMenu/MainMenu.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.SceneManagement;
+
+using UnityEngine.UI;
+using TMPro;
+
+public class MainMenu : MonoBehaviour
+{
+    [SerializeField]
+    private ButtonScene playButton;
+
+    [SerializeField]
+    private ButtonScene editButton;
+
+    private void Awake()
+    {
+        playButton.Init();
+        editButton.Init();
+    }
+
+    [System.Serializable]
+    private class ButtonScene
+    {
+        [SerializeField]
+        private Button button;
+
+        [SerializeField]
+        private string sceneGoToName;
+
+        public void Init()
+        {
+            button.onClick.AddListener(MoveToScene);
+        }
+
+        private void MoveToScene()
+        {
+            SceneManager.LoadScene(sceneGoToName, LoadSceneMode.Single);
+        }
+    }
+}
