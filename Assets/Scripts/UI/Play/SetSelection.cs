7b1d9ec6822deedf8807a42e46e4f15e1f456d1b
Simon Hyltegard <simon.home@hotmail.com>
2020-11-30 09:40:49 +0100

    Added set selection

---
+++ Assets/Scripts/UI/Play/SetSelection.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.UI;
+using TMPro;
+
+namespace Play
+{
+    public class SetSelection : MonoBehaviour
+    {
+        [SerializeField]
+        private RectTransform interfaceHolder;
+
+        [SerializeField]
+        private SetSelectionInterface setSelectionInterfacePrefab;
+
+        private void OnEnable()
+        {
+            List<Transform> children = Utility.GetFirstLevelChildren(interfaceHolder);
+            for (int i = 0; i < children.Count; i++)
+            {
+                Destroy(children[i].gameObject);
+            }
+
+            List<string> setNameList = QuestionManager.GetAllSetNames();
+            for (int i = 0; i < setNameList.Count; i++)
+            {
+                GameObject ob = Instantiate(setSelectionInterfacePrefab.gameObject);
+                ob.transform.SetParent(interfaceHolder);
+                ob.GetComponent<SetSelectionInterface>().Set(QuestionManager.GetSet(setNameList[i]), i == 0);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/UI/Play/SetSelectionInterface.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.UI;
+using TMPro;
+
+namespace Play
+{
+    public class SetSelectionInterface : MonoBehaviour
+    {
+        private QuestionManager.QuestionSet questionSet;
+
+        [SerializeField]
+        private TextMeshProUGUI questionSetNameText;
+
+        [SerializeField]
+        private Button toggleButton;
+
+        [SerializeField]
+        private RectTransform[] onGraphics;
+
+        [SerializeField]
+        private RectTransform[] offGraphics;
+
+        private bool isOn;
+
+        private void Awake()
+        {
+            toggleButton.onClick.AddListener(delegate { this.IsActive = !this.IsActive; });
+        }
+
+        public void Set(QuestionManager.QuestionSet questionSet, bool toggled)
+        {
+            this.questionSet = questionSet;
+
+            this.questionSetNameText.text = this.questionSet.GetDisplayName();
+            this.IsActive = toggled;
+        }
+
+        public bool IsActive
+        {
+            get
+            {
+                return this.isOn;
+            }
+
+            private set
+            {
+                this.isOn = value;
+
+                for (int i = 0; i < onGraphics.Length; i++)
+                {
+                    onGraphics[i].gameObject.SetActive(this.isOn);
+                }
+
+                for (int i = 0; i < offGraphics.Length; i++)
+                {
+                    offGraphics[i].gameObject.SetActive(!this.isOn);
+                }
+            }
+        }
+    }
+}
