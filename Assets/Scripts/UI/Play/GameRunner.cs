1a489af07f4ca56fc584dcf09b7e66fb743b1aea
Simon Hyltegard <simon.home@hotmail.com>
2020-12-25 20:10:31 +0100

    Improved play graphics

--- Assets/Scripts/UI/Play/GameRunner.cs
+++ Assets/Scripts/UI/Play/GameRunner.cs
@@ -42,7 +42,7 @@ namespace Play
         {
             if (questionList.Count == 0)
             {
-                ConfirmScreen.Create().Set("You have played through all the questions, do you want to restart?", Replay, PlayHandler.BackPhase, useCancel: true, confirmText: "Replay");
+                ConfirmScreen.Create().Set("Play Again?", Replay, PlayHandler.BackPhase, useCancel: true, confirmText: "Replay");
                 return;
             }
 
