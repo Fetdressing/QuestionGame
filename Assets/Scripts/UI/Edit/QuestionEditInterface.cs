7654766984502cceea5261aff07e36be278b4f59
Simon Hyltegard <simon.home@hotmail.com>
2020-12-26 16:05:51 +0100

    Made sure text input shows multi line when typing longer texts

--- Assets/Scripts/UI/Edit/QuestionEditInterface.cs
+++ Assets/Scripts/UI/Edit/QuestionEditInterface.cs
@@ -38,7 +38,7 @@ namespace Edit
             }
 
             string startValue = string.Equals(questionEditInterface.Question.value, QuestionManager.emptyQuestion) ? "" : questionEditInterface.Question.value;
-            InputScreen.Create().Set("Change question: ", 3, 50, true, (newValue) => { OnChangeQuestion(questionEditInterface, newValue); }, startValue: startValue);
+            InputScreen.Create().Set("Change question: ", 3, 70, true, (newValue) => { OnChangeQuestion(questionEditInterface, newValue); }, startValue: startValue);
         }
 
         private void OnChangeQuestion(QuestionEditInterface questionEditInterface, string newValue)
